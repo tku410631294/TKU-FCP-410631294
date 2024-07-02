b8a51c063e805defa5dc9d5609bfa104bfe7fb3c
tku410631294 <410631294@gms.tku.edu.tw>
2024-07-02 15:30:00 +0800

    更新

---
+++ test1.java
@@ -0,0 +1,9 @@
+public class test1 {
+    
+    public static void main(String[] args) {
+        System.out.println("easy come , easy go");
+        System.out.println("practime makes perfect");
+        
+
+    }
+}
---
+++ test2.java
@@ -0,0 +1,18 @@
+
+
+
+public class test2 {
+
+    public static void main(String[] args) {
+        
+        int a = 12;
+        int b = 26;
+        float k = 12.26f;
+        System.out.println((a+b));
+        System.out.println(k);
+
+        
+
+    }
+    
+}
