357f15d6a8da69ef32cb941d01a0c3f13ed6b976
tku410631294 <410631294@gms.tku.edu.tw>
2024-07-02 11:52:34 +0800

    我的第一支java程式

---
+++ q1.java
@@ -0,0 +1,11 @@
+
+
+public class q1 {
+
+    public static void main(String[] args) {
+        System.out.println("hello java");
+        System.out.println("0702");
+    }
+
+    
+}
---
+++ q2.java
@@ -0,0 +1,19 @@
+public class q2 {    //定義public類別 q2
+
+    public static void main(String[] args) {    //main() 函數 主程式開始
+        int num = 2;  //宣告整數num
+        int num2 = 7;
+        float num3 = 45;
+        String num4 = "456644";
+
+        System.out.println(num+" cat are running");  //印出字串及變數內容
+        System.out.println(num2+" dog are running");
+        System.out.println(num3+" dog are running");
+        System.out.println(num4+" dog are running");
+
+
+
+
+    }
+    
+}
