dc5bdc3eca248e6ae9022a67023e4564d983cf9a
tku410631294 <410631294@gms.tku.edu.tw>
2024-07-03 14:13:43 +0800

    1

--- hm_04.java
+++
@@ -1,9 +0,0 @@
-package hw;
-
-public class hm_04 {
-    public static void main(String[] args) {
-        int a =10,b=5;
-        System.out.println("和:"+(a+b) +" 差:"+(a-b) +" 積:"+(a*b) +" 商:"+(a/b) );
-    }
-
-}
