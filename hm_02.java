7d09f1e8dd9bdf3ae633949de584710692b0fb6b
tku410631294 <410631294@gms.tku.edu.tw>
2024-07-03 13:48:11 +0800

    Add files via upload

---
+++ hm_02.java
@@ -0,0 +1,15 @@
+package hw;
+
+public class hm_02 {
+    public static void main(String[] args) {
+        System.out.println("黃柏翔");
+        int age= 21;
+        System.out.println("年齡"+age);
+        System.out.println("興趣:健身");
+        
+
+
+
+    }
+    
+}
