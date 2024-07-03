ce6687c974703bf3ac94cddea37a3c379c5f3b52
tku410631294 <410631294@gms.tku.edu.tw>
2024-07-03 10:58:53 +0800

    更新

---
+++ class0703.java
@@ -0,0 +1,13 @@
+public class class0703 { //定義類別
+    public static void main(String[] args) {
+        int a ; //宣告變數a,但不設定值
+        int b=4,sum; //宣告b和sum兩個變數,但只將b的值設為4
+
+        a=5;  //設定a的值為5
+        sum=a+b; //將sum設為a+b
+        System.out.println("sum = "+sum);  //印出sum
+
+
+    }
+    
+}
